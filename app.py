9df5b0a1d813e967384ebd5bd11e5a39f63042a1
a20n21 <andersonmatias188@outlook.com>
2026-06-01 18:04:40 -0300

    teste pipeline automático

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {
-        "application": "Azure Container Apps",
+        "application": "Azure Container Apps Teste",
         "status": "running"
     }
 
